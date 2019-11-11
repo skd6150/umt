13f68711e41f4dd7369bebddfea8b272f65410ea
An Yong Mo <aym506@hotmail.co.kr>
2019-11-12 00:56:35 +0900

    TextInput 삽입

--- frontend/components/ReportSwitch/ReportTab.js
+++ frontend/components/ReportSwitch/ReportTab.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
+import { View, Text, StyleSheet } from 'react-native';
+import { TextInput } from 'react-native-paper';
 
 export default class ReportTab extends Component 
 {
@@ -8,12 +8,33 @@ export default class ReportTab extends Component
     static navigationOptions = {
         header: null
     }
+
+    // 신고를 위해 입력되는 값들
+    state = {
+        phonenumber: ""
+    }
     
     // '신고' 탭에 표시되는 내용
     render() {
         return (
             <View style={styles.container}>
-                <Text>신고 ㄱㄱ</Text>
+                <View>
+                <TextInput
+                    label='배달부의 전화번호'
+                    value={''}
+                    onChangeText={text => this.setState({ phonenumber })}
+                />
+                </View>
+                <View>
+
+                </View>
+                <View>
+                <TextInput
+                    label='메뉴'
+                    value={''}
+                    onChangeText={text => this.setState({ phonenumber })}
+                />
+                </View>
             </View>
         );
     }
@@ -25,7 +46,8 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        marginTop: 20,
+        marginVertical: 20,
         marginHorizontal: 10
-    }
+    },
+
 });
